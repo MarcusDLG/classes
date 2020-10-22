84cd00750bff4bb3a43f46bff11fee58a748636e
MarcusDLG <delag010@gmail.com>
2020-10-22 11:25:39 -0400

    created mapping

---
+++ programming101/mapping.py
@@ -0,0 +1,21 @@
+superhero = {
+    "name": "Wonder Woman",
+    "alias": "Diana Prince",
+    "gear": [
+        "Lasso of Truth",
+        "Bracelets of Submission"
+    ],
+    "vehicle": {
+        "title": "Invisible Jet",
+        "speed": "2000 mph",
+    }
+}
+
+superhero["alias"] = "Princess Diana of Themyscira"
+superhero["hometown"] = "Themyscira"
+print(superhero["hometown"])
+for key, value in superhero.items():
+    print("Superhero's %s is %s" % (key, value))
+
+for item in superhero["gear"]:
+    print("%s has %s" % (superhero["name"], item))
