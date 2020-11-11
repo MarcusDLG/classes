5deb137082c046e449fa1299fb31d40ebb51bf5f
MarcusDLG <delag010@gmail.com>
2020-11-11 12:28:01 -0500

    working through JSON

--- javascript/json.js
+++ javascript/json.js
@@ -1,3 +1,11 @@
+let myJsonObject = `{
+  "name":"Marcus",
+  "age":31,
+  "email":"marcus@iconicdigitalpalette.com"
+}`;
+let oneJSONobject = JSON.stringify(myJsonObject);
+console.log(oneJSONobject); //Create a javascript object and convert that object into a JSON string.
+
 let arrayOfObjectJSON = `[
   {
       "name":"Marcus",
@@ -24,5 +32,5 @@ let famArray = JSON.parse(arrayOfObjectJSON);
 let famArray2 = JSON.stringify(arrayOfObjectJSON);
 let famArray3 = JSON.parse(famArray2);
 console.log(famArray);
-console.log(famArray2);
-console.log(famArray3);
+console.log(famArray2); //Create an array of objects and turn that array of objects into a JSON string
+console.log(famArray3); //Turn that JSON string back into a an array of objects.
