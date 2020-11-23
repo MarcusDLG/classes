d32b5060902651ff837feed1f7c1524771feb644
MarcusDLG <delag010@gmail.com>
2020-11-23 13:10:07 -0600

    updates

--- Node/read-file.js
+++ Node/read-file.js
@@ -4,13 +4,23 @@ const fs = require("fs");
 //   console.log(data);
 // });
 
-// fs.readFile("data.json", "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(JSON.parse(data));
-// });
+//this will print out as a JSON object but will not return until the two Synchronous functions below are complete. See next note
+fs.readFile("data.json", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(JSON.parse(data));
+});
 
+//13-17 will execute simultaneously with 7-10, but due to the Sync on 13-17
+//it will actually queue 7-10 and will not let 7-10 finish its return until 13-17 are complete.
 let data = fs.readFileSync("data.json", "utf8");
 let dataArr = JSON.parse(data);
 dataArr.map((value, index) => {
   console.log(`${value.name} is ${value.age} years old`);
+});
+
+//same as 13-17, but utilizing the index to number the people.
+let dataData = fs.readFileSync("data.json", "utf8");
+let dataDataArr = JSON.parse(dataData);
+dataDataArr.map((value, index) => {
+  console.log(`${index + 1}. ${value.name} is ${value.age} years old`);
 });
